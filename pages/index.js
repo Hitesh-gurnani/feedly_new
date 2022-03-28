5b4b83db7aebb7dca2398d2a2a4b0140b6b25548
Hitesh-gurnani <hiteshgurnani850@gmail.com>
2022-03-28 10:50:49 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,50 @@ import Image from 'next/image'
 
 import Link from 'next/link'
 import Layout from '../component/Layout'
-export default function Home() {
+import { API_URL } from '../config/index'
+import NewsItem from '../component/NewsItem'
+export default function Home({ news }) {
+  // {
+  //   console.log()
+  // }
   return (
     <div >
       <Layout>
-
-        <h1>Home</h1>
-        <Link href="/about">
+        <h1>Latest News</h1>
+        {
+          news.length === 0 && <h3>No news</h3>
+        }
+        {
+          news.map((item) => (
+            <NewsItem key={item.id} news={item} />
+          ))
+        }
+        {news.length > 0 && (
+          <Link href='/news'>
+            <a className='btn-secondary'>View All news</a>
+          </Link>
+        )}
+        {/* <Link href="/about">
           <a>About</a>
-        </Link>
+        </Link> */}
       </Layout>
     </div>
   )
 }
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/news/data`)
+  // console.log(res);
+  const news = await res.json();
+  return {
+    props: { news: news.slice(0, 5) },
+  }
+}
+// export async function getStaticProps() {
+//   const res = await fetch(`${API_URL}/api/news/data`)
+//   // console.log(res);
+//   const news = await res.json();
+//   return {
+//     props: { news },
+//   }
+// }
